9c514ff3b01bfe02a6010c7cc269f21640d4a77a
Nadiia Miziukanova <mizukanovanadya@gmail.com>
2026-07-14 16:05:00 +0300

    Solution 2.0

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 def copy_file(command: str) -> None:
     parts = command.split(" ")
 
-    if len(parts) < 3:
+    if len(parts) < 3 or parts[0] != "cp":
         return
 
     if parts[1] == parts[2]:
         return
 
-    if parts[0] == "cp":
-        try:
-            with open(
-                parts[1],
-                "r"
-            ) as origin_file, open(
-                parts[2],
-                "w"
-            ) as copy_file:
-                copy_file.write(origin_file.read())
-        except FileNotFoundError:
-            return
+    try:
+        with open(
+            parts[1],
+            "r"
+        ) as origin_file, open(
+            parts[2],
+            "w"
+        ) as copy_file:
+            copy_file.write(origin_file.read())
+    except FileNotFoundError:
+        return
